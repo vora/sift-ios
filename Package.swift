072892b7919d05535763fe4195924a4647d7058e
Kyle Lanchman <kyle.lanchman@ascendum.com>
2021-03-26 16:43:44 -0400

    Trying to get SPM working

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "sift-ios",
+    name: "Sift",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "sift-ios",
-            targets: ["sift-ios"]),
+            name: "Sift",
+            targets: ["Sift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sift-ios",
-            dependencies: []),
+            name: "Sift",
+            dependencies: [],
+            path: "Sift",
+            exclude: ["Info.plist", "README.md"],
+            publicHeadersPath: "."),
         .testTarget(
             name: "sift-iosTests",
-            dependencies: ["sift-ios"]),
+            dependencies: ["Sift"]),
     ]
 )
